b8e9b887bbfb4ae055185cad6722e4e97a35f4dd
Symuciakas <sixas10@gmail.com>
2020-03-05 00:31:04 +0200

    Added basic stuff

---
+++ Assets/playercontroller.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playercontroller : MonoBehaviour
+{
+    public Rigidbody2D rb;
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.A) /*&& rb.velocity.y == 0*/)
+        {
+            rb.velocity = new Vector2(-5, rb.velocity.y);
+        }
+        if (Input.GetKey(KeyCode.D) /*&& rb.velocity.y == 0*/)
+        {
+            rb.velocity = new Vector2(5, rb.velocity.y);
+        }
+        if (Input.GetKey(KeyCode.W) && rb.velocity.y == 0)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, 10f);
+        }
+    }
+}
