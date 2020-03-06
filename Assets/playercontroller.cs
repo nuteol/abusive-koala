8da1eedb733cbb34887d85dba9ea69dade0a84bc
Mr-Farty <easteregg489@gmail.com>
2020-03-06 16:17:03 +0200

    Added Walk, Jump, Idle animations
    https://abusivekoala.atlassian.net/jira/software/projects/TR/boards/1?selectedIssue=TR-10
    
    Added all walk, jump, idle animations with temporary sprites. Also changed the way the characters sprite looks when he goes to left or the right. Still have to add the Jump finish animation. Also stretched the collision box for the character temporaraly becouse of the new sprites.

--- Assets/playercontroller.cs
+++ Assets/playercontroller.cs
@@ -5,20 +5,38 @@ using UnityEngine;
 public class playercontroller : MonoBehaviour
 {
     public Rigidbody2D rb;
+    public Animator animator;
 
+    public float runSpeed = 40f;
+    float horizontalMove = 0f;
     private void Update()
     {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
         if (Input.GetKey(KeyCode.A) /*&& rb.velocity.y == 0*/)
         {
             rb.velocity = new Vector2(-5, rb.velocity.y);
+            transform.eulerAngles = new Vector3(0, 180, 0); //character direction
+
         }
         if (Input.GetKey(KeyCode.D) /*&& rb.velocity.y == 0*/)
         {
             rb.velocity = new Vector2(5, rb.velocity.y);
+            transform.eulerAngles = new Vector3(0, 0, 0);
         }
         if (Input.GetKey(KeyCode.W) && rb.velocity.y == 0)
         {
             rb.velocity = new Vector2(rb.velocity.x, 10f);
+            animator.SetBool("IsJumping", true);
         }
+        if(Input.GetKey(KeyCode.S))
+        {
+            
+        }
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
     }
+    void OnLanding()
+    {
+        animator.SetBool("IsJumping", false);
+    }
+
 }
