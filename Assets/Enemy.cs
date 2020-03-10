c8ba246588376dd8b689e99cc886dc48d205dcfb
Symuciakas <sixas10@gmail.com>
2020-03-11 01:02:04 +0200

    Added monster object and attack with x

---
+++ Assets/Enemy.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int maxHealth = 10;
+    private int currentHealth;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        print(currentHealth);
+        if(currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        print("Cuntified");
+    }
+}
---
+++ Assets/attackingscript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class attackingscript : MonoBehaviour
+{
+    private float nextAttackTime = 0f;
+    public Transform attackPos;
+    public LayerMask whatIsEnemies;
+    public float attackRange;
+    public int damage;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Time.time >= nextAttackTime)
+        {
+            if (Input.GetKey(KeyCode.X))
+            {
+                Collider2D[] enemiesToDamage = Physics2D.OverlapCircleAll(attackPos.position, attackRange, whatIsEnemies);
+                foreach (Collider2D enemy in enemiesToDamage)
+                {
+                    enemy.GetComponent<Enemy>().TakeDamage(damage);
+                }
+                nextAttackTime = Time.time + 1f;
+            }
+        }
+    }
+}
